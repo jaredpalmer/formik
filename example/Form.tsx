6ea9945b68b112ff87ea8aa9e6520b551f8e9eb0
Jared Palmer <jared@palmer.net>
2017-06-16 15:57:46 -0400

    Update example

--- example/Form.tsx
+++ example/Form.tsx
@@ -21,40 +21,7 @@ interface FormPayload {
   data: FormValues;
 }
 
-function Form({
-  onSubmit,
-  values: { email, firstName, facebook },
-  onChange,
-}: InjectedFormikProps<FormValues>) {
-  return (
-    <form onSubmit={onSubmit}>
-      <input
-        type="text"
-        name="email"
-        value={email}
-        onChange={onChange}
-        placeholder="john@apple.com"
-      />
-      <input
-        type="text"
-        name="firstName"
-        value={firstName}
-        onChange={onChange}
-        placeholder="John"
-      />
-      <input
-        type="text"
-        name="facebook"
-        value={facebook}
-        onChange={onChange}
-        placeholder="facebook username"
-      />
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
-
-export default Formik<FormProps, FormValues, FormPayload>({
+const withFormik = Formik<FormProps, FormValues, FormPayload>({
   displayName: 'TestForm',
   mapPropsToValues: ({ email, firstName, social }) => ({
     email,
@@ -68,19 +35,54 @@ export default Formik<FormProps, FormValues, FormPayload>({
     facebook: yup.string(),
   }),
   handleSubmit: (payload, { props, setSubmitting }) => {
-    setSubmitting(true);
-    callMyApi(payload)
-      .then(res => {
+    callMyApi(payload).then(
+      res => {
         setSubmitting(false);
         MyToast.show({
-          message: 'Success!'
+          message: 'Success!',
         });
-      }, err => => {
+      },
+      err => {
         setSubmitting(false);
         MyToast.show({
-          message: 'Error!'
+          message: 'Error!',
         });
-      });
+      }
+    );
   },
- 
-})(Form as any);
+});
+
+function MyForm({
+  handleSubmit,
+  values: { email, firstName, facebook },
+  handleChange,
+}: InjectedFormikProps<FormValues>) {
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="email"
+        value={email}
+        onChange={handleChange}
+        placeholder="john@apple.com"
+      />
+      <input
+        type="text"
+        name="firstName"
+        value={firstName}
+        onChange={handleChange}
+        placeholder="John"
+      />
+      <input
+        type="text"
+        name="facebook"
+        value={facebook}
+        onChange={handleChange}
+        placeholder="facebook username"
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default withFormik(MyForm);

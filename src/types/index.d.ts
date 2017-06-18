7f6fc315eda14e7455492ea551935e27ad398a88
Jared Palmer <jared@palmer.net>
2017-06-18 14:53:08 -0400

    Add types for hoist-non-react-statics

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,1 +1,21 @@
-declare module 'yup';
+declare module 'yup' {
+
+}
+
+declare module 'hoist-non-react-statics' {
+  /**
+   * Copies any static properties present on `source` to `target`, excluding those that are specific
+   * to React.
+   *
+   * Returns the target component.
+   */
+  function hoistNonReactStatics(
+    targetComponent: any,
+    sourceComponent: any,
+    customStatics: { [name: string]: boolean }
+  ): any;
+  namespace hoistNonReactStatics {
+
+  }
+  export = hoistNonReactStatics;
+}

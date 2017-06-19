04140b261968509d64d7592286c1bd9ba92d4789
Jared Palmer <jared@palmer.net>
2017-06-19 10:32:01 -0400

    Set defaults for InjectedFormikProp, remove fixtures

--- src/__tests__/Formik.test.tsx
+++ src/__tests__/Formik.test.tsx
@@ -1,28 +1,74 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 
-import { Simple, SimpleProps } from '../__fixtures__/Simple';
+import Formik, { InjectedFormikProps } from '../.';
 
-import Formik from '../.';
 import Yup from 'yup';
 
 describe('Formik', () => {
   it('renders Formik correctly', () => {
-    const Form = Formik<SimpleProps, SimpleProps, SimpleProps>({
+    interface Props { thing: string }
+
+    const Form: React.SFC<InjectedFormikProps<Props>> = ({
+      values,
+      handleSubmit,
+      handleChange,
+      errors,
+    }) => {
+      return (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={handleChange}
+            value={values.thing}
+            name="thing"
+          />
+          {errors.thing && <div>{errors.thing}</div>}
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    };
+
+    const FormikEnhancer = Formik<Props>({
       validationSchema: Yup.object().shape({
         thing: Yup.string(),
       }),
       handleSubmit: payload => {
         console.log(payload);
       },
-    })(Simple as any);
+    });
 
-    const tree = renderer.create(<Form thing="hello" />).toJSON();
+    const EnhancedForm = FormikEnhancer(Form);
+
+    const tree = renderer.create(<EnhancedForm thing="hello" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('mapsPropsToValues', () => {
-    const Form = Formik<{ hello: string }, SimpleProps, SimpleProps>({
+    interface Props { hello: string }
+    interface Values { thing: string }
+
+    const Form: React.SFC<InjectedFormikProps<Props, Values>> = ({
+      values,
+      handleSubmit,
+      handleChange,
+      errors,
+    }) => {
+      return (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={handleChange}
+            value={values.thing}
+            name="thing"
+          />
+          {errors.thing && <div>{errors.thing}</div>}
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    };
+
+    const FormikEnhancer = Formik<Props, Values>({
       mapPropsToValues: ({ hello }) => ({ thing: hello }),
       validationSchema: Yup.object().shape({
         thing: Yup.string(),
@@ -30,9 +76,11 @@ describe('Formik', () => {
       handleSubmit: payload => {
         console.log(payload);
       },
-    })(Simple as any);
+    });
 
-    const tree = renderer.create(<Form hello="hello" />).toJSON();
+    const EnhancedForm = FormikEnhancer(Form);
+
+    const tree = renderer.create(<EnhancedForm hello="hello" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });

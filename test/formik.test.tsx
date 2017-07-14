354c5ef945eb0a6c86938372289a70aa5ea648e8
Jared Palmer <jared@palmer.net>
2017-07-14 15:43:04 -0400

    Clean up tests

--- test/formik.test.tsx
+++ test/formik.test.tsx
@@ -9,157 +9,137 @@ const Yup = require('yup');
 // tslint:disable-next-line:no-empty
 const noop = () => {};
 
-describe('Formik', () => {
-  interface Props {
-    user: {
-      name: string;
-    };
-    someFunction?: () => void;
-  }
-
-  interface Values {
+interface Props {
+  user: {
     name: string;
-  }
-
-  const Form: React.SFC<InjectedFormikProps<Props, Values>> = ({
-    values,
-    handleSubmit,
-    handleChange,
-    handleBlur,
-    setStatus,
-    status,
-    errors,
-    isSubmitting,
-  }) => {
-    return (
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.name}
-          name="name"
-        />
-        {errors.name &&
-          <div id="feedback">
-            {errors.name}
-          </div>}
-        {isSubmitting && <div id="submitting">Submitting</div>}
-        <button
-          id="statusButton"
-          onClick={() => setStatus({ myStatusMessage: 'True' })}
-        >
-          Call setStatus
-        </button>
-        {status &&
-          !!status.myStatusMessage &&
-          <div id="statusMessage">
-            {status.myStatusMessage}
-          </div>}
-        <button type="submit">Submit</button>
-      </form>
-    );
   };
+  someFunction?: () => void;
+}
 
-  it('initializes Formik state', () => {
-    const EnhancedForm = Formik<Props, Values, Values>({
-      validationSchema: Yup.object().shape({
-        name: Yup.string(),
-      }),
-      mapPropsToValues: ({ user }) => ({ ...user }),
-      handleSubmit: noop,
-    })(Form);
-    const hoc = mount(<EnhancedForm user={{ name: 'jared' }} />);
+interface Values {
+  name: string;
+}
+
+const Form: React.SFC<InjectedFormikProps<Props, Values>> = ({
+  values,
+  handleSubmit,
+  handleChange,
+  handleBlur,
+  setStatus,
+  status,
+  errors,
+  isSubmitting,
+}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        value={values.name}
+        name="name"
+      />
+      {errors.name &&
+        <div id="feedback">
+          {errors.name}
+        </div>}
+      {isSubmitting && <div id="submitting">Submitting</div>}
+      <button
+        id="statusButton"
+        onClick={() => setStatus({ myStatusMessage: 'True' })}
+      >
+        Call setStatus
+      </button>
+      {status &&
+        !!status.myStatusMessage &&
+        <div id="statusMessage">
+          {status.myStatusMessage}
+        </div>}
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+const BasicForm = Formik<Props, Values, Values>({
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required(),
+  }),
+  mapPropsToValues: ({ user }) => ({ ...user }),
+  handleSubmit: noop,
+})(Form);
+
+describe('Formik', () => {
+  it('should initialize Formik state', () => {
+    const hoc = mount(<BasicForm user={{ name: 'jared' }} />);
     expect(hoc.state().isSubmitting).toBe(false);
     expect(hoc.state().touched).toEqual({});
     expect(hoc.state().values).toEqual({ name: 'jared' });
     expect(hoc.state().errors).toEqual({});
   });
 
-  describe('handleSubmit', () => {
-    it('calls preventDefault()', () => {
-      const EnhancedForm = Formik<Props, Values, Values>({
-        validationSchema: Yup.object().shape({
-          name: Yup.string(),
-        }),
-        mapPropsToValues: ({ user }) => ({ ...user }),
-        handleSubmit: noop,
-      })(Form);
-      const hoc = mount(<EnhancedForm user={{ name: 'jared' }} />);
-      const preventDefault = jest.fn();
-      hoc.find('form').simulate('submit', { preventDefault });
-      expect(preventDefault).toHaveBeenCalled();
-    });
-
-    it('touches all fields', () => {
-      const EnhancedForm = Formik<Props, Values, Values>({
-        validationSchema: Yup.object().shape({
-          name: Yup.string(),
-        }),
-        mapPropsToValues: ({ user }) => ({ ...user }),
-        handleSubmit: noop,
-      })(Form);
-      const hoc = mount(<EnhancedForm user={{ name: 'jared' }} />);
-      hoc.find('form').simulate('submit');
-      expect(hoc.state().isSubmitting).toBe(true);
-      expect(hoc.state().touched).toEqual({ name: true });
-    });
-
-    it('pushes submission state chagnes to child component', async () => {
-      const EnhancedForm = Formik<Props, Values, Values>({
-        validationSchema: Yup.object().shape({
-          name: Yup.string().required(),
-        }),
-        mapPropsToValues: ({ user }) => ({ ...user }),
-        handleSubmit: noop,
-      })(Form);
-      const hoc = shallow(<EnhancedForm user={{ name: 'jared' }} />);
-
-      expect(hoc.find(Form).dive().find('#submitting')).toHaveLength(0);
-
-      hoc.find(Form).dive().find('form').simulate('submit', {
-        // tslint:disable-next-line:no-empty
-        preventDefault() {},
+  describe('FormikHandlers', () => {
+    describe('handleSubmit', () => {
+      it('should call preventDefault()', () => {
+        const hoc = mount(<BasicForm user={{ name: 'jared' }} />);
+        const preventDefault = jest.fn();
+        hoc.find('form').simulate('submit', { preventDefault });
+        expect(preventDefault).toHaveBeenCalled();
       });
-      expect(hoc.find(Form).dive().find('#submitting')).toHaveLength(1);
-    });
 
-    it('can map values to payload', async () => {
-      interface Payload {
-        user: { name: string };
-      }
-      const EnhancedForm = Formik<Props, Values, Payload>({
-        validationSchema: Yup.object().shape({
-          name: Yup.string().required(),
-        }),
-        mapPropsToValues: ({ user }) => ({ ...user }),
-        mapValuesToPayload: ({ name }) => ({ user: { name } }),
-        handleSubmit: payload => {
-          expect(payload).toEqual({ user: { name: 'jared' } });
-          expect(payload).not.toEqual({ name: 'jared' });
-        },
-      })(Form);
-      const hoc = shallow(<EnhancedForm user={{ name: 'jared' }} />);
-      hoc.find(Form).dive().find('form').simulate('submit', {
-        // tslint:disable-next-line:no-empty
-        preventDefault() {},
+      it('should touch all fields', () => {
+        const hoc = mount(<BasicForm user={{ name: 'jared' }} />);
+        hoc.find('form').simulate('submit');
+        expect(hoc.state().isSubmitting).toBe(true);
+        expect(hoc.state().touched).toEqual({ name: true });
+      });
+
+      it('should push submission state changes to child component', async () => {
+        const hoc = shallow(<BasicForm user={{ name: 'jared' }} />);
+
+        expect(hoc.find(Form).dive().find('#submitting')).toHaveLength(0);
+
+        hoc.find(Form).dive().find('form').simulate('submit', {
+          // tslint:disable-next-line:no-empty
+          preventDefault() {},
+        });
+        expect(hoc.find(Form).dive().find('#submitting')).toHaveLength(1);
+      });
+
+      it('should correctly map form values to payload', async () => {
+        interface Payload {
+          user: { name: string };
+        }
+        const CustomPayloadForm = Formik<Props, Values, Payload>({
+          validationSchema: Yup.object().shape({
+            name: Yup.string().required(),
+          }),
+          mapPropsToValues: ({ user }) => ({ ...user }),
+          mapValuesToPayload: ({ name }) => ({ user: { name } }),
+          handleSubmit: payload => {
+            expect(payload).toEqual({ user: { name: 'jared' } });
+            expect(payload).not.toEqual({ name: 'jared' });
+          },
+        })(Form);
+        const hoc = shallow(<CustomPayloadForm user={{ name: 'jared' }} />);
+        hoc.find(Form).dive().find('form').simulate('submit', {
+          // tslint:disable-next-line:no-empty
+          preventDefault() {},
+        });
       });
     });
-  });
 
-  describe('formikBag', () => {
+    const CustomConfigForm = Formik<Props, Values, Values>({
+      validationSchema: Yup.object().shape({
+        name: Yup.string().required(),
+      }),
+      mapPropsToValues: ({ user }) => ({ ...user }),
+      handleSubmit: payload => {
+        expect(payload).toEqual({ name: 'jared' });
+      },
+    })(Form);
+
     it('handleBlur sets touched', async () => {
-      const EnhancedForm = Formik<Props, Values, Values>({
-        validationSchema: Yup.object().shape({
-          name: Yup.string().required(),
-        }),
-        mapPropsToValues: ({ user }) => ({ ...user }),
-        handleSubmit: payload => {
-          expect(payload).toEqual({ name: 'jared' });
-        },
-      })(Form);
-
-      const hoc = shallow(<EnhancedForm user={{ name: 'jared' }} />);
+      const hoc = shallow(<CustomConfigForm user={{ name: 'jared' }} />);
 
       // Simulate a blur event in the inner Form component's input
       hoc.find(Form).dive().find('input').simulate('blur', {
@@ -172,17 +152,7 @@ describe('Formik', () => {
     });
 
     it('handleChange sets values, and updates inputs', async () => {
-      const EnhancedForm = Formik<Props, Values, Values>({
-        validationSchema: Yup.object().shape({
-          name: Yup.string().required(),
-        }),
-        mapPropsToValues: ({ user }) => ({ ...user }),
-        handleSubmit: payload => {
-          expect(payload).toEqual({ name: 'jared' });
-        },
-      })(Form);
-
-      const hoc = shallow(<EnhancedForm user={{ name: 'jared' }} />);
+      const hoc = shallow(<CustomConfigForm user={{ name: 'jared' }} />);
 
       // Simulate a change event in the inner Form component's input
       hoc.find(Form).dive().find('input').simulate('change', {
@@ -198,45 +168,23 @@ describe('Formik', () => {
         hoc.update().find(Form).dive().find('input').props().value
       ).toEqual('ian');
     });
+  });
 
+  describe('FormikActions', () => {
     it('setValues sets values', async () => {
-      const EnhancedForm = Formik<Props, Values, Values>({
-        validationSchema: Yup.object().shape({
-          name: Yup.string().required(),
-        }),
-        mapPropsToValues: ({ user }) => ({ ...user }),
-        handleSubmit: noop,
-      })(Form);
-
-      const hoc = mount(<EnhancedForm user={{ name: 'jared' }} />);
+      const hoc = mount(<BasicForm user={{ name: 'jared' }} />);
       hoc.find(Form).props().setValues({ name: 'ian' });
       expect(hoc.find('input').props().value).toEqual('ian');
     });
 
     it('setErrors sets error object', async () => {
-      const EnhancedForm = Formik<Props, Values, Values>({
-        validationSchema: Yup.object().shape({
-          name: Yup.string().required(),
-        }),
-        mapPropsToValues: ({ user }) => ({ ...user }),
-        handleSubmit: noop,
-      })(Form);
-
-      const hoc = mount(<EnhancedForm user={{ name: 'jared' }} />);
+      const hoc = mount(<BasicForm user={{ name: 'jared' }} />);
       hoc.find(Form).props().setErrors({ name: 'Required' });
       expect(hoc.find('#feedback').text()).toEqual('Required');
     });
 
     it('setStatus sets status object', async () => {
-      const EnhancedForm = Formik<Props, Values, Values>({
-        validationSchema: Yup.object().shape({
-          name: Yup.string().required(),
-        }),
-        mapPropsToValues: ({ user }) => ({ ...user }),
-        handleSubmit: noop,
-      })(Form);
-
-      const hoc = shallow(<EnhancedForm user={{ name: 'jared' }} />);
+      const hoc = shallow(<BasicForm user={{ name: 'jared' }} />);
       hoc.find(Form).dive().find('#statusButton').simulate('click');
       expect(hoc.find(Form).dive().find('#statusMessage')).toHaveLength(1);
     });

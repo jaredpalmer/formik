46f8404177ebd1a1b7836704cee3b660c5a76cf1
Jared Palmer <jared@palmer.net>
2017-08-05 23:33:49 -0400

    Simplify rollup config and build npm script

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,56 @@
+import commonjs from 'rollup-plugin-commonjs';
+import filesize from 'rollup-plugin-filesize';
+import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import sourceMaps from 'rollup-plugin-sourcemaps';
+import uglify from 'rollup-plugin-uglify';
+
+const shared = {
+  entry: `compiled/formik.js`,
+  sourceMap: true,
+  external: ['react', 'prop-types'],
+  globals: {
+    react: 'React',
+    'prop-types': 'PropTypes',
+  },
+  exports: 'named',
+};
 
 export default [
-  {
-    entry: `compiled/formik.js`,
+  Object.assign({}, shared, {
+    moduleName: 'Formik',
+    format: 'umd',
+    dest:
+      process.env.NODE_ENV === 'production'
+        ? './dist/formik.umd.min.js'
+        : './dist/formik.umd.js',
+    plugins: [
+      resolve(),
+      replace({
+        exclude: 'node_modules/**',
+        'process.env.NODE_ENV': JSON.stringify(
+          process.env.NODE_ENV || 'development'
+        ),
+      }),
+      commonjs(),
+      sourceMaps(),
+      process.env.NODE_ENV === 'production' && filesize(),
+      process.env.NODE_ENV === 'production' && uglify(),
+    ],
+  }),
+  Object.assign({}, shared, {
     targets: [
       { dest: 'dist/formik.es6.js', format: 'es' },
       { dest: 'dist/formik.js', format: 'cjs' },
     ],
-    sourceMap: true,
-    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-    external: ['react', 'prop-types'],
-    globals: {
-      react: 'React',
-      'prop-types': 'PropTypes',
-    },
-    exports: 'named',
-    plugins: [
-      // Allow node_modules resolution, so you can use 'external' to control
-      // which external modules to include in the bundle
-      // https://github.com/rollup/rollup-plugin-node-resolve#usage
-      resolve(),
-      // Resolve source maps to the original source
-      sourceMaps(),
-    ],
-  },
-  {
+    plugins: [resolve(), sourceMaps()],
+  }),
+  Object.assign({}, shared, {
     entry: `compiled/next.js`,
     targets: [
       { dest: 'dist/next.es6.js', format: 'es' },
       { dest: 'dist/next.js', format: 'cjs' },
     ],
-    sourceMap: true,
-    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-    external: ['react', 'prop-types'],
-    globals: {
-      react: 'React',
-      'prop-types': 'PropTypes',
-    },
-    exports: 'named',
-    plugins: [
-      // Allow node_modules resolution, so you can use 'external' to control
-      // which external modules to include in the bundle
-      // https://github.com/rollup/rollup-plugin-node-resolve#usage
-      resolve(),
-      // Resolve source maps to the original source
-      sourceMaps(),
-    ],
-  },
+    plugins: [resolve(), sourceMaps()],
+  }),
 ];

4d6e9b9044818342fda9fd3aba44bc5b9ced5deb
Brian Ng <bng412@gmail.com>
2018-01-09 17:06:08 -0600

    Fix unshift in FieldArray

--- test/FieldArray.test.tsx
+++ test/FieldArray.test.tsx
@@ -7,9 +7,12 @@ import { isFunction } from '../src/utils';
 // tslint:disable-next-line:no-empty
 const noop = () => {};
 
-const friends = ['jared', 'andrea', 'brent'];
 const TestForm: React.SFC<any> = p => (
-  <Formik onSubmit={noop} initialValues={{ friends }} {...p} />
+  <Formik
+    onSubmit={noop}
+    initialValues={{ friends: ['jared', 'andrea', 'brent'] }}
+    {...p}
+  />
 );
 
 describe('<FieldArray />', () => {
@@ -59,8 +62,8 @@ describe('<FieldArray />', () => {
       );
 
       arrayHelpers.push('jared');
-      expect(formikBag.values.friends).toEqual(friends.concat('jared'));
-      expect(formikBag.values.friends.length).toEqual(friends.length + 1);
+      const expected = ['jared', 'andrea', 'brent', 'jared'];
+      expect(formikBag.values.friends).toEqual(expected);
     });
   });
 
@@ -87,8 +90,122 @@ describe('<FieldArray />', () => {
       );
 
       const el = arrayHelpers.pop();
-      expect(formikBag.values.friends).toEqual(['jared', 'andrea']);
+      const expected = ['jared', 'andrea'];
+      expect(formikBag.values.friends).toEqual(expected);
       expect(el).toEqual('brent');
+    });
+  });
+
+  describe('props.swap()', () => {
+    it('should swap two values in field array', () => {
+      let formikBag: any;
+      let arrayHelpers: any;
+      ReactDOM.render(
+        <TestForm
+          render={(props: any) => {
+            formikBag = props;
+            return (
+              <FieldArray
+                name="friends"
+                render={arrayProps => {
+                  arrayHelpers = arrayProps;
+                  return null;
+                }}
+              />
+            );
+          }}
+        />,
+        node
+      );
+
+      arrayHelpers.swap(0, 2);
+      const expected = ['brent', 'andrea', 'jared'];
+      expect(formikBag.values.friends).toEqual(expected);
+    });
+  });
+
+  describe('props.insert()', () => {
+    it('should insert a value at given index of field array', () => {
+      let formikBag: any;
+      let arrayHelpers: any;
+      ReactDOM.render(
+        <TestForm
+          render={(props: any) => {
+            formikBag = props;
+            return (
+              <FieldArray
+                name="friends"
+                render={arrayProps => {
+                  arrayHelpers = arrayProps;
+                  return null;
+                }}
+              />
+            );
+          }}
+        />,
+        node
+      );
+
+      arrayHelpers.insert(1, 'brian');
+      const expected = ['jared', 'brian', 'andrea', 'brent'];
+      expect(formikBag.values.friends).toEqual(expected);
+    });
+  });
+
+  describe('props.unshift()', () => {
+    it('should add a value to start of field array and return its length', () => {
+      let formikBag: any;
+      let arrayHelpers: any;
+      ReactDOM.render(
+        <TestForm
+          render={(props: any) => {
+            formikBag = props;
+            return (
+              <FieldArray
+                name="friends"
+                render={arrayProps => {
+                  arrayHelpers = arrayProps;
+                  return null;
+                }}
+              />
+            );
+          }}
+        />,
+        node
+      );
+
+      const el = arrayHelpers.unshift('brian');
+      const expected = ['brian', 'jared', 'andrea', 'brent'];
+      expect(formikBag.values.friends).toEqual(expected);
+      expect(el).toEqual(4);
+    });
+  });
+
+  describe('props.remove()', () => {
+    it('should remove a value at given index of field array', () => {
+      let formikBag: any;
+      let arrayHelpers: any;
+      ReactDOM.render(
+        <TestForm
+          render={(props: any) => {
+            formikBag = props;
+            return (
+              <FieldArray
+                name="friends"
+                render={arrayProps => {
+                  arrayHelpers = arrayProps;
+                  return null;
+                }}
+              />
+            );
+          }}
+        />,
+        node
+      );
+
+      arrayHelpers.remove(1);
+      const expected = ['jared', 'brent'];
+      expect(formikBag.values.friends).toEqual(expected);
     });
   });
 });

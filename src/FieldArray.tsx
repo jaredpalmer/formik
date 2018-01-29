b32ebfd3f294996f88634bdaa9574f158274bb49
Jared Palmer <jared@palmer.net>
2018-01-29 16:44:35 -0500

    #378 remove setFieldTouched from FieldArray

--- src/FieldArray.tsx
+++ src/FieldArray.tsx
@@ -57,11 +57,10 @@ export class FieldArray extends React.Component<FieldArrayConfig, {}> {
   };
 
   changeValue = (fn: Function) => {
-    const { setFieldValue, setFieldTouched, values } = this.context.formik;
+    const { setFieldValue, values } = this.context.formik;
     const { name } = this.props;
     const val = fn(dlv(values, name));
     setFieldValue(name, val);
-    setFieldTouched(name, true);
   };
 
   push = (value: any) => this.changeValue((array: any[]) => [...array, value]);

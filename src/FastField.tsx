2df2a808e0819368589ff015a2162985ca6c93e1
Jared Palmer <jared@palmer.net>
2018-02-21 11:05:55 -0500

    Add the ability to handle codependent validation to FastField with diff

--- src/FastField.tsx
+++ src/FastField.tsx
@@ -14,7 +14,8 @@ export interface FastFieldState {
   touched: boolean;
 }
 
-function diffObjectsExceptForKey(a: any, b: any, path: string) {
+/** @private Returns whether two objects are deeply equal **excluding** a key / dot path */
+function isEqualExceptForKey(a: any, b: any, path: string) {
   return isEqual(setIn(a, path, undefined), setIn(b, path, undefined));
 }
 
@@ -87,6 +88,7 @@ export class FastField<
       values,
       validationSchema,
       errors,
+      setFormikState,
     } = this.context.formik;
     const { type, value, checked } = e.target;
     const val = /number|range/.test(type)
@@ -95,7 +97,6 @@ export class FastField<
     if (validateOnChange) {
       // Field-level validation
       if (this.props.validate) {
-        console.log('field-validate');
         const maybePromise = (this.props.validate as any)(value);
         if (isPromise(maybePromise)) {
           this.setState({ value: val });
@@ -111,25 +112,44 @@ export class FastField<
         const maybePromise = (validate as any)(
           setIn(values, this.props.name, val)
         );
-        console.log('validate');
-        console.log(
-          diffObjectsExceptForKey(maybePromise, errors, this.props.name)
-        );
+
         if (isPromise(maybePromise)) {
           this.setState({ value: val });
           (maybePromise as any).then(
             () => this.setState({ error: undefined }),
-            (error: any) =>
-              this.setState({ error: getIn(error, this.props.name) })
+            (error: any) => {
+              // Here we diff the errors object relative to Formik parents except for
+              // the Field's key. If they are equal, the field's validation function is
+              // has no inter-field side-effects and we only need to update local state
+              // otherwise we need to lift up the update to the parent (causing a full form render)
+              if (isEqualExceptForKey(maybePromise, errors, this.props.name)) {
+                this.setState({ error: getIn(error, this.props.name) });
+              } else {
+                setFormikState((prevState: any) => ({
+                  ...prevState,
+                  errors: error,
+                  // touched: setIn(prevState.touched, name, true),
+                }));
+              }
+            }
           );
         } else {
-          console.log(
-            diffObjectsExceptForKey(maybePromise, errors, this.props.name)
-          );
-          this.setState({
-            value: val,
-            error: getIn(maybePromise, this.props.name),
-          });
+          // Handle the same diff situation
+          // @todo refactor
+          if (isEqualExceptForKey(maybePromise, errors, this.props.name)) {
+            this.setState({
+              value: val,
+              error: getIn(maybePromise, this.props.name),
+            });
+          } else {
+            this.setState({
+              value: val,
+            });
+            setFormikState((prevState: any) => ({
+              ...prevState,
+              errors: maybePromise,
+            }));
+          }
         }
       } else if (validationSchema) {
         // Top-level validationsSchema
@@ -241,7 +261,11 @@ export class FastField<
     };
 
     if (render) {
-      return (render as any)(bag);
+      return (render as (
+        props: FieldProps<any> & {
+          meta: { error?: string; touched?: boolean };
+        }
+      ) => React.ReactNode)(bag);
     }
 
     if (isFunction(children)) {

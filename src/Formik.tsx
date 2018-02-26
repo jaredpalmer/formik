e75fe773b00fed4900b03e1faa75ad0361969cdf
Shane Tenorio <sltenorio99@gmail.com>
2018-02-26 00:20:45 -0600

    Put activeelement blur in submitForm(), passes tests

--- src/Formik.tsx
+++ src/Formik.tsx
@@ -494,19 +494,18 @@ export class Formik<ExtraProps = {}, Values = object> extends React.Component<
     if (e && e.preventDefault) {
       e.preventDefault();
     }
+    this.submitForm();
+  };
+
+  submitForm = () => {
+    //Blur active element first
     if (
       document.activeElement &&
       document.activeElement instanceof HTMLElement
     ) {
       document.activeElement.blur();
     }
-    // Queues this.submitForm so that it's called after the active element's blur event's code
-    setTimeout(() => {
-      this.submitForm();
-    }, 0);
-  };
 
-  submitForm = () => {
     // Recursively set all values to `true`.
     this.setState({
       touched: setNestedObjectValues<FormikTouched<Values>>(

833f273a5d7beccc7cd5ab1723cef17519f797f0
ohbarye <over.rye@gmail.com>
2018-04-10 22:57:16 +0900

    Remove duplicated interfaces
    Those interfaces are added in
    https://github.com/jaredpalmer/formik/pull/141 and still remains.

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,5 @@
 import * as React from 'react';
 
-export type CompositeComponent<P> =
-  | React.ComponentClass<P>
-  | React.StatelessComponent<P>;
-
-export interface ComponentDecorator<TOwnProps, TMergedProps> {
-  (component: CompositeComponent<TMergedProps>): React.ComponentClass<
-    TOwnProps
-  >;
-}
-
-export interface InferableComponentDecorator<TOwnProps> {
-  <T extends CompositeComponent<TOwnProps>>(component: T): T;
-}
-
 export interface SharedRenderProps<T> {
   /**
    * Field component to render. Can either be a string like 'select' or a component.

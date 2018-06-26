679be4b968d0c9c757a5e42ee3a21cf0946b9cb0
yanchith <yanchi.toth@gmail.com>
2018-06-26 19:48:58 +1000

    Update example to properly control props

--- website/src/examples/AsyncValidationOuter.js
+++ website/src/examples/AsyncValidationOuter.js
@@ -14,6 +14,9 @@ const validate = values => {
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
     errors.email = 'Invalid email address';
   }
+  if (!values.password || values.password.length < 3) {
+    errors.password = 'Password must be at least 3 characters long'
+  }
   return errors;
 };
 
@@ -21,7 +24,10 @@ class SignIn extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { errors: {} };
+    this.state = {
+      errors: {},
+      values: { email: '', password: '' },
+    };
   }
 
   render() {
@@ -29,10 +35,7 @@ class SignIn extends React.Component {
       <div>
         <h1>Sign In</h1>
         <Formik
-          initialValues={{
-            email: '',
-            password: '',
-          }}
+          initialValues={this.state.values}
           initialErrors={this.state.errors}
           enableReinitialize={true}
           validate={validate}
@@ -52,6 +55,10 @@ class SignIn extends React.Component {
               <label htmlFor="password">Password</label>
               <Field name="password" type="password" />
 
+              {errors.password && touched.password &&
+                <div className="field-error">{errors.password}</div>
+              }
+
               <button type="submit">Sign In</button>
             </Form>
           )}
@@ -62,9 +69,15 @@ class SignIn extends React.Component {
 
   handleSubmit = (values) => {
     if (EXISTING_EMAILS.indexOf(values.email) === -1) {
-      this.setState({ errors: { email: 'That account does not exist' } });
+      this.setState((prevState) => ({
+        errors: { email: 'That account does not exist' },
+        values: { ...prevState.values, email: values.email },
+      }));
     } else {
-      this.setState({ errors: {} });
+      this.setState((prevState) => ({
+        errors: {},
+        values: { ...prevState.values, email: values.email },
+      }));
     }
   }
 }

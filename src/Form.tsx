fb6484487df32e09d8aa5935e0810fe29e6a1d63
Tim Griesser <tgriesser10@gmail.com>
2018-07-03 13:27:11 -0400

    Replace 'any' on <Form /> with proper types

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import { connect } from './connect';
 
-export const Form = connect<any>(({ formik: { handleSubmit }, ...props }) => (
-  <form onSubmit={handleSubmit} {...props} />
-));
+export type FormikFormProps = Pick<
+  React.FormHTMLAttributes<HTMLFormElement>,
+  Exclude<keyof React.FormHTMLAttributes<HTMLFormElement>, 'onSubmit'>
+>;
+
+export const Form = connect<FormikFormProps>(
+  ({ formik: { handleSubmit }, ...props }) => (
+    <form onSubmit={handleSubmit} {...props} />
+  )
+);
 
 Form.displayName = 'Form';

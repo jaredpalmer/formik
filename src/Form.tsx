8280418ade5bd36f9bcd840d6f9cc98dbb56bbe8
Jared Palmer <jaredloganpalmer@gmail.com>
2018-07-03 14:47:14 -0400

    Revert "Replace 'any' on <Form /> with proper types (#730)"
    This reverts commit 7fc86da2114933b59f0176ab42a9aa07fb6a5647.

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,15 +1,8 @@
 import * as React from 'react';
 import { connect } from './connect';
 
-export type FormikFormProps = Pick<
-  React.FormHTMLAttributes<HTMLFormElement>,
-  Exclude<keyof React.FormHTMLAttributes<HTMLFormElement>, 'onSubmit'>
->;
-
-export const Form = connect<FormikFormProps>(
-  ({ formik: { handleSubmit }, ...props }) => (
-    <form onSubmit={handleSubmit} {...props} />
-  )
-);
+export const Form = connect<any>(({ formik: { handleSubmit }, ...props }) => (
+  <form onSubmit={handleSubmit} {...props} />
+));
 
 Form.displayName = 'Form';

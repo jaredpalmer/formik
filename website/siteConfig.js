196db28533d09d449bee5e9f341b7e1e8238595a
Jared Palmer <jared@palmer.net>
2018-09-04 16:41:52 -0400

    Fix copyright

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -114,7 +114,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Jared Palmer or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} Jared Palmer`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.

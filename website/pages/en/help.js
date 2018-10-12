d592f2b4de85e377254341c45ed2614c825e17fd
Osamah Aldoaiss <osamah@aldoaiss.de>
2018-10-12 13:34:26 +0200

    fix url to overview page (#993)

--- website/pages/en/help.js
+++ website/pages/en/help.js
@@ -15,7 +15,9 @@ const GridBlock = CompLibrary.GridBlock;
 const siteConfig = require(`${process.cwd()}/siteConfig.js`);
 
 function docUrl(doc, language) {
-  return `${siteConfig.baseUrl}docs/${language ? `${language}/` : ''}${doc}`;
+  return `${siteConfig.baseUrl}docs/${
+    language && language !== 'en' ? `${language}/` : ''
+  }${doc}`;
 }
 
 class Help extends React.Component {

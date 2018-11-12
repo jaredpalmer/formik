37fdde52dee879a628b2961abe8a3256c1bdf7ae
David Babel <DavidBabel@users.noreply.github.com>
2018-11-12 10:26:18 +0100

    Update FastField.tsx

--- src/FastField.tsx
+++ src/FastField.tsx
@@ -111,18 +111,18 @@ class FastFieldInner<Values = {}, Props = {}> extends React.Component<
     if (this.props.shouldUpdate) {
       return this.props.shouldUpdate(props, this.props);
     } else if (
-      getIn(this.props.formik.values, this.props.name) !==
-        getIn(props.formik.values, this.props.name) ||
-      getIn(this.props.formik.errors, this.props.name) !==
-        getIn(props.formik.errors, this.props.name) ||
-      getIn(this.props.formik.touched, this.props.name) !==
-        getIn(props.formik.touched, this.props.name) ||
-      Object.keys(this.props).length !== Object.keys(props).length ||
-      this.props.formik.isSubmitting !== props.formik.isSubmitting
+      getIn(this.props.formik.values, this.props.name) ===
+        getIn(props.formik.values, this.props.name) &&
+      getIn(this.props.formik.errors, this.props.name) ===
+        getIn(props.formik.errors, this.props.name) &&
+      getIn(this.props.formik.touched, this.props.name) ===
+        getIn(props.formik.touched, this.props.name) &&
+      Object.keys(this.props).length === Object.keys(props).length &&
+      this.props.formik.isSubmitting === props.formik.isSubmitting
     ) {
-      return true;
-    } else {
       return false;
+    } else {
+      return true;
     }
   }
 

2c7415fea2738b7d7160b007a7f0bb19df349fdc
Sajjad Hashemian <wolaws@gmail.com>
2019-02-21 05:30:06 +0330

    fix warnings

--- src/Formik.tsx
+++ src/Formik.tsx
@@ -315,10 +315,8 @@ export function useFormik<Values = object>({
         activeElement instanceof HTMLButtonElement
       ) {
         warning(
-          !!(
-            activeElement.attributes &&
-            activeElement.attributes.getNamedItem('type')
-          ),
+          activeElement.attributes &&
+            activeElement.attributes.getNamedItem('type'),
           'You submitted a Formik form using a button with an unspecified `type` attribute.  Most browsers default button elements to `type="submit"`. If this is not a submit button, please add `type="button"`.'
         );
       }

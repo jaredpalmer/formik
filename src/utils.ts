0b9780f3abbcf03bc37814c01e831b47312da821
Jared Palmer <jared@palmer.net>
2019-04-26 11:21:14 -0400

    Add exhaustive comment

--- src/utils.ts
+++ src/utils.ts
@@ -99,11 +99,31 @@ export function getIn(
 }
 
 /**
- * Deeply set a value from in object via its path.
+ * Deeply set a value from in object via it's path. If the value at `path`
+ * has changed, return a shallow copy of obj with `value` set at `path`.
+ * If `value` has not changed, return the original `obj`.
+ *
+ * Existing objects / arrays along `path` are also shallow copied. Sibling
+ * objects along path retain the same internal js reference. Since new
+ * objects / arrays are only created along `path`, we can test if anything
+ * changed in a nested structure by comparing the object's reference in
+ * the old and new object, similar to how russian doll cache invalidation
+ * works.
+ *
+ * In earlier versions of this function, which used cloneDeep, there were
+ * issues whereby settings a nested value would mutate the parent
+ * instead of creating a new object. `clone` avoids that bug making a
+ * shallow copy of the objects along the update path
+ * so no object is mutated in place.
+ *
+ * Before changing this function, please read through the following
+ * discussions.
+ *
  * @see https://github.com/developit/linkstate
+ * @see https://github.com/jaredpalmer/formik/pull/123
  */
 export function setIn(obj: any, path: string, value: any): any {
-  let res: any = clone(obj);
+  let res: any = clone(obj); // this keeps inheritance when obj is a class
   let resVal: any = res;
   let i = 0;
   let pathArray = toPath(path);

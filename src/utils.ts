d3da70a453f16b84dcd643ebac2eebdf46a7c60a
Jared Palmer <jared@palmer.net>
2019-04-26 10:58:48 -0400

    Allow setIn to keep class inheritance

--- src/utils.ts
+++ src/utils.ts
@@ -2,97 +2,6 @@ import clone from 'lodash/clone';
 import toPath from 'lodash/toPath';
 import * as React from 'react';
 
-/**
- * Deeply get a value from an object via its path.
- */
-export function getIn(
-  obj: any,
-  key: string | string[],
-  def?: any,
-  p: number = 0
-) {
-  const path = toPath(key);
-  while (obj && p < path.length) {
-    obj = obj[path[p++]];
-  }
-  return obj === undefined ? def : obj;
-}
-
-/**
- * Deeply set a value from in object via its path.
- * @see https://github.com/developit/linkstate
- */
-export function setIn(obj: any, path: string, value: any): any {
-  let res: any = cloneDeep(obj);
-  let resVal: any = res;
-  let i = 0;
-  let pathArray = toPath(path);
-
-  for (; i < pathArray.length - 1; i++) {
-    const currentPath: string = pathArray[i];
-    let currentObj: any = getIn(obj, pathArray.slice(0, i + 1));
-
-    if (currentObj) {
-      resVal = resVal[currentPath] = clone(currentObj);
-    } else {
-      const nextPath: string = pathArray[i + 1];
-      resVal = resVal[currentPath] =
-        isInteger(nextPath) && Number(nextPath) >= 0 ? [] : {};
-    }
-  }
-
-  // Return original object if new value is the same as current
-  if ((i === 0 ? obj : resVal)[pathArray[i]] === value) {
-    return obj;
-  }
-
-  if (value === undefined) {
-    delete resVal[pathArray[i]];
-  } else {
-    resVal[pathArray[i]] = value;
-  }
-
-  // If the path array has a single element, the loop did not run.
-  // Deleting on `resVal` had no effect in this scenario, so we delete on the result instead.
-  if (i === 0 && value === undefined) {
-    delete res[pathArray[i]];
-  }
-
-  return res;
-}
-
-/**
- * Recursively a set the same value for all keys and arrays nested object, cloning
- * @param object
- * @param value
- * @param visited
- * @param response
- */
-export function setNestedObjectValues<T>(
-  object: any,
-  value: any,
-  visited: any = new WeakMap(),
-  response: any = {}
-): T {
-  for (let k of Object.keys(object)) {
-    const val = object[k];
-    if (isObject(val)) {
-      if (!visited.get(val)) {
-        visited.set(val, true);
-        // In order to keep array values consistent for both dot path  and
-        // bracket syntax, we need to check if this is an array so that
-        // this will output  { friends: [true] } and not { friends: { "0": true } }
-        response[k] = Array.isArray(val) ? [] : {};
-        setNestedObjectValues(val, value, visited, response[k]);
-      }
-    } else {
-      response[k] = value;
-    }
-  }
-
-  return response;
-}
-
 // Assertions
 
 /** @private is the given object a Function? */
@@ -171,4 +80,95 @@ export function makeCancelable<T extends Promise<any>>(
       hasCanceled = true;
     },
   ];
+}
+
+/**
+ * Deeply get a value from an object via its path.
+ */
+export function getIn(
+  obj: any,
+  key: string | string[],
+  def?: any,
+  p: number = 0
+) {
+  const path = toPath(key);
+  while (obj && p < path.length) {
+    obj = obj[path[p++]];
+  }
+  return obj === undefined ? def : obj;
+}
+
+/**
+ * Deeply set a value from in object via its path.
+ * @see https://github.com/developit/linkstate
+ */
+export function setIn(obj: any, path: string, value: any): any {
+  let res: any = clone(obj);
+  let resVal: any = res;
+  let i = 0;
+  let pathArray = toPath(path);
+
+  for (; i < pathArray.length - 1; i++) {
+    const currentPath: string = pathArray[i];
+    let currentObj: any = getIn(obj, pathArray.slice(0, i + 1));
+
+    if (currentObj) {
+      resVal = resVal[currentPath] = clone(currentObj);
+    } else {
+      const nextPath: string = pathArray[i + 1];
+      resVal = resVal[currentPath] =
+        isInteger(nextPath) && Number(nextPath) >= 0 ? [] : {};
+    }
+  }
+
+  // Return original object if new value is the same as current
+  if ((i === 0 ? obj : resVal)[pathArray[i]] === value) {
+    return obj;
+  }
+
+  if (value === undefined) {
+    delete resVal[pathArray[i]];
+  } else {
+    resVal[pathArray[i]] = value;
+  }
+
+  // If the path array has a single element, the loop did not run.
+  // Deleting on `resVal` had no effect in this scenario, so we delete on the result instead.
+  if (i === 0 && value === undefined) {
+    delete res[pathArray[i]];
+  }
+
+  return res;
+}
+
+/**
+ * Recursively a set the same value for all keys and arrays nested object, cloning
+ * @param object
+ * @param value
+ * @param visited
+ * @param response
+ */
+export function setNestedObjectValues<T>(
+  object: any,
+  value: any,
+  visited: any = new WeakMap(),
+  response: any = {}
+): T {
+  for (let k of Object.keys(object)) {
+    const val = object[k];
+    if (isObject(val)) {
+      if (!visited.get(val)) {
+        visited.set(val, true);
+        // In order to keep array values consistent for both dot path  and
+        // bracket syntax, we need to check if this is an array so that
+        // this will output  { friends: [true] } and not { friends: { "0": true } }
+        response[k] = Array.isArray(val) ? [] : {};
+        setNestedObjectValues(val, value, visited, response[k]);
+      }
+    } else {
+      response[k] = value;
+    }
+  }
+
+  return response;
 }

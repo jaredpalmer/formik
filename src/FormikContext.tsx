ebd07fe908281c607ed3ed7791baa4ecf2c8b7d3
Jared Palmer <jared@palmer.net>
2019-06-22 14:43:23 -0400

    Warn when using useFormikContext() without ctx

--- src/FormikContext.tsx
+++ src/FormikContext.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
 import { FormikContext } from './types';
+import invariant from 'tiny-warning';
 
 const PrivateFormikContext = React.createContext<FormikContext<any>>({} as any);
 export const FormikProvider = PrivateFormikContext.Provider;
 export const FormikConsumer = PrivateFormikContext.Consumer;
 
 export function useFormikContext<Values>() {
-  return React.useContext<FormikContext<Values>>(PrivateFormikContext);
+  const formik = React.useContext<FormikContext<Values>>(PrivateFormikContext);
+
+  invariant(
+    !formik,
+    `Formik context is undefined, please verify you are calling useFormikContext() as child of a <Formik> component.`
+  );
+
+  return formik;
 }

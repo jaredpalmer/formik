e3ff274d7a929c0827a22374b42e11f912ab5a34
stevenm-alintaenergy <MASOS0@alintaenergy.com.au>
2019-11-15 12:13:53 +1100

    Fix setFieldValue types for nested objects and arrays

--- src/types.tsx
+++ src/types.tsx
@@ -87,15 +87,15 @@ export interface FormikHelpers<Values> {
   setValues(values: Values): void;
   /** Set value of form field directly */
   setFieldValue(
-    field: keyof Values & string,
+    field: keyof Values | string,
     value: any,
     shouldValidate?: boolean
   ): void;
   /** Set error message of a form field directly */
-  setFieldError(field: keyof Values & string, message: string): void;
+  setFieldError(field: keyof Values | string, message: string): void;
   /** Set whether field has been touched directly */
   setFieldTouched(
-    field: keyof Values & string,
+    field: keyof Values | string,
     isTouched?: boolean,
     shouldValidate?: boolean
   ): void;

daf4f11a53d1336434580ec00c863b23a0be1686
Jared Palmer <jaredloganpalmer@gmail.com>
2019-12-10 16:56:02 -0500

    Move to monorepo (#2102)
    * Lerna, stub formik-native
    
    * Fix test command
    
    * Move now.json into the root so docs can deploy
    
    * Symlink the root readme for now
    
    * Symlink migration guide
    
    * Add lerna bootstrap to postinstall
    
    * Specify multiple packages in codesandbox ci
    
    * Fix now builds
    
    * Remove codesandbox build command
    
    * Fix both CS and Now build commands
    
    * Pass test flags through to tsdx packages
    
    * Fix gitignore

---
+++ packages/formik-native/README.md
@@ -0,0 +1,7 @@
+# Formik Native
+
+```
+yarn add formik formik-native
+```
+
+This is an experimental package. DO NOT USE IN PRODUCTION.
---
+++ packages/formik-native/test/blah.test.ts
@@ -0,0 +1,7 @@
+import { sum } from '../src';
+
+describe('blah', () => {
+  it('works', () => {
+    expect(sum(1, 1)).toEqual(2);
+  });
+});

0c803ba8dc5d304e140ce46feb06a6df71a79685
Jared Palmer <jared@palmer.net>
2020-01-28 16:38:46 -0500

    Add checkbox example

---
+++ examples/checkboxes/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Formik, Field, Form } from 'formik';
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+const Basic = () => (
+  <div>
+    <h1>Sign Up</h1>
+    <Formik
+      initialValues={{
+        toggle: false,
+        checked: [],
+      }}
+      onSubmit={async values => {
+        await sleep(500);
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      {({ values }) => (
+        <Form>
+          {/* 
+            This first checkbox will result in a boolean value being stored. Note that the `value` prop
+            on the <Field/> is omitted
+          */}
+          <label>
+            <Field type="checkbox" name="toggle" />
+            {`${values.toggle}`}
+          </label>
+
+          {/* 
+            Multiple checkboxes with the same name attribute, but different
+            value attributes will be considered a "checkbox group". Formik will automagically
+            bind the checked values to a single array for your benefit. All the add and remove
+            logic will be taken care of for you.
+          */}
+          <div id="checkbox-group">Checked</div>
+          <div role="group" aria-labeledby="checkbox-group">
+            <label>
+              <Field type="checkbox" name="checked" value="One" />
+              One
+            </label>
+            <label>
+              <Field type="checkbox" name="checked" value="Two" />
+              Two
+            </label>
+            <label>
+              <Field type="checkbox" name="checked" value="Three" />
+              Three
+            </label>
+          </div>
+
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
+
+ReactDOM.render(<Basic />, document.getElementById('root'));

8bd2b5482d2de0d9867bcfd555a7f60b1502a824
Jared Palmer <jared@palmer.net>
2020-01-28 14:26:54 -0500

    Add basic-typescript codesandbox example folder

---
+++ examples/basic-typescript/index.tsx
@@ -0,0 +1,54 @@
+import 'react-app-polyfill/ie11';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Formik, Field, Form, FormikHelpers } from 'formik';
+
+interface Values {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Signup</h1>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+        }}
+        onSubmit={(
+          values: Values,
+          { setSubmitting }: FormikHelpers<Values>
+        ) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 500);
+        }}
+      >
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field id="firstName" name="firstName" placeholder="John" />
+
+          <label htmlFor="lastName">Last Name</label>
+          <Field id="lastName" name="lastName" placeholder="Doe" />
+
+          <label htmlFor="email">Email</label>
+          <Field
+            id="email"
+            name="email"
+            placeholder="john@acme.com"
+            type="email"
+          />
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));

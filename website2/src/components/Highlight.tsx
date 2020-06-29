eed24847ef25f1a6dd1fde95f3a1aaf234241289
Jared Palmer <jared@palmer.net>
2020-06-29 17:14:57 -0400

    Fix line numbers

--- website2/src/components/Highlight.tsx
+++ website2/src/components/Highlight.tsx
@@ -1,29 +1,3 @@
-// import * as React from 'react';
-// import HighlightJS from 'react-highlight';
-
-// export default function Highlight({
-//   children,
-//   className,
-//   style,
-// }: {
-//   children: React.ReactNode;
-//   className: string;
-//   style?: React.CSSProperties;
-// }) {
-//   return (
-//     <div className="rounded-lg p-6 bg-gray-800 overflow-x-auto text-gray-400 text-sm leading-tight">
-//       <HighlightJS
-//         className={`language-${
-//           className && className.replace(/language-/, '')
-//         }`}
-//         {...style}
-//       >
-//         {children}
-//       </HighlightJS>
-//     </div>
-//   );
-// }
-
 import * as React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
@@ -181,12 +155,16 @@ const Code = ({
           >
             {tokens.map((line, i) => (
               <div key={i} {...getLineProps({ line, key: i })}>
-                <span
-                  aria-hidden="true"
-                  className="select-none text-gray-300 text-right w-5 inline-block mx-2"
-                >
-                  {i}
-                </span>{' '}
+                {tokens.length > 1 ? (
+                  <span
+                    aria-hidden="true"
+                    className="select-none text-gray-300 text-right w-5 inline-block mx-2"
+                  >
+                    {i + 1}
+                  </span>
+                ) : (
+                  <span className="mx-2 w-5" />
+                )}{' '}
                 {line.map((token, key) => (
                   <span key={key} {...getTokenProps({ token, key })} />
                 ))}

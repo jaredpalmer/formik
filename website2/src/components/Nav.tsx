f9f858084f0467c1a79a5f16f04d098911e1ac85
Jared Palmer <jared@palmer.net>
2020-06-30 20:36:38 -0400

    Remove unused imports

--- website2/src/components/Nav.tsx
+++ website2/src/components/Nav.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react';
 import Link from 'next/link';
 import { Logo } from './Logo';
-import { AnimatePresence, motion } from 'framer-motion';
-import cn from 'classnames';
 import { siteConfig } from 'siteConfig';
-import ExternalLink from './blog/ExternalLink';
 
 export const Nav: React.FC = () => (
   <div className="bg-white border-b border-gray-200">

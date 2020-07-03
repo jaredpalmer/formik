28a1f523ee5411f0757a9826af791ca51793012b
Jared Palmer <jared@palmer.net>
2020-07-02 20:08:53 -0400

    Lazy load the code block

--- docs/src/components/MDXComponents.tsx
+++ docs/src/components/MDXComponents.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import dynamic from 'next/dynamic';
-import Highlight from './Highlight';
+
 export default {
   // default tags
   pre: (p: any) => <pre {...p} />,
-  code: Highlight,
+  code: dynamic(() => import('./Highlight2')),
   // Counter: dynamic(() => import('./counter')),
 };

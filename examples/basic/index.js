89fb9a67fdfd91192b3113d195e049d8d07e8d39
Jared Palmer <jared@palmer.net>
2020-07-14 13:38:12 -0400

    Make basic example more a11y

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -1,36 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Formik, Field, Form } from "formik";
-
-const sleep = ms => new Promise(r => setTimeout(r, ms));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Formik, Field, Form } from 'formik';
 
 const Basic = () => (
   <div>
     <h1>Sign Up</h1>
     <Formik
       initialValues={{
-        firstName: "",
-        lastName: "",
-        email: ""
+        firstName: '',
+        lastName: '',
+        email: '',
       }}
       onSubmit={async values => {
-        await sleep(500);
+        await new Promise(r => setTimeout(r, 500));
         alert(JSON.stringify(values, null, 2));
       }}
     >
       <Form>
         <label htmlFor="firstName">First Name</label>
-        <Field name="firstName" placeholder="Jane" />
+        <Field id="firstName" name="firstName" placeholder="Jane" />
 
         <label htmlFor="lastName">Last Name</label>
-        <Field name="lastName" placeholder="Doe" />
+        <Field id="lastName" name="lastName" placeholder="Doe" />
 
         <label htmlFor="email">Email</label>
-        <Field name="email" placeholder="jane@acme.com" type="email" />
+        <Field
+          id="email"
+          name="email"
+          placeholder="jane@acme.com"
+          type="email"
+        />
         <button type="submit">Submit</button>
       </Form>
     </Formik>
   </div>
 );
 
-ReactDOM.render(<Basic />, document.getElementById("root"));
+ReactDOM.render(<Basic />, document.getElementById('root'));

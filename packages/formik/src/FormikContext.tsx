403747392a85dedc20ee5a2bbf3e46b45265b145
John Rom <john@nimblelight.com>
2021-03-11 18:13:58 -0500

    Wire up FormikConsumer.

--- packages/formik/src/FormikContext.tsx
+++ packages/formik/src/FormikContext.tsx
@@ -3,6 +3,7 @@ import { FormikContextType } from './types';
 import invariant from 'tiny-warning';
 import { useFormikApi } from './hooks/useFormikApi';
 import { useFullFormikState } from './hooks/useFullFormikState';
+import { FormikConnectedType } from './connect';
 
 export const FormikContext = React.createContext<FormikContextType<any>>(
   undefined as any
@@ -10,12 +11,11 @@ export const FormikContext = React.createContext<FormikContextType<any>>(
 FormikContext.displayName = 'FormikContext';
 
 export const FormikProvider = FormikContext.Provider;
-export const FormikConsumer = FormikContext.Consumer;
 
 /**
  * @deprecated Please use useFormikApi() and access state from api.useState().
  */
-export function useFormikContext<Values>() {
+ export function useFormikContext<Values>() {
   const formik = useFormikApi<Values>();
 
   invariant(
@@ -30,3 +30,21 @@ export function useFormikContext<Values>() {
     ...state,
   };
 }
+
+/**
+ * @deprecated Please use useFormikApi() and access state from api.useState().
+ */
+export function FormikConsumer<Values = any>({
+  children,
+}: {
+  children: (formik: FormikConnectedType<Values>) => React.ReactNode;
+}) {
+  const formik = useFormikContext<Values>();
+
+  invariant(
+    !!formik,
+    `Formik context is undefined, please verify you are calling useFormikContext() as child of a <Formik> component.`
+  );
+
+  return <>{children(formik)}</>;
+};

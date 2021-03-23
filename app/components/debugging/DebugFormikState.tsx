0079948e40ef15548d6d22b48a843446c11e9faf
John Rom <web@johnrom.com>
2021-03-23 15:43:43 -0400

    Remove unused helper components from app/

--- app/components/debugging/DebugFormikState.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { useFormikContext } from 'formik';
-import { DebugProps } from './DebugProps';
-
-export const DebugFormikState = () => {
-  const formikState = useFormikContext();
-
-  return <DebugProps {...formikState} />;
-};

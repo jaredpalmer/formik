4b76e2bd2a2d0e180fa58e451a47301764540173
John Rom <john@nimblelight.com>
2021-08-12 13:46:52 -0400

    RN is not detecting .native suffix.

--- packages/formik/src/index.tsx
+++ packages/formik/src/index.tsx
@@ -3,7 +3,7 @@ export * from './exports';
 // Borrowed from React-Redux, License MIT
 // https://github.com/reduxjs/react-redux/blob/d4e4eba9ccbd488b103b3c5625a37e15b1427d11/src/alternate-renderers.ts
 // https://github.com/reduxjs/react-redux/blob/d4e4eba9ccbd488b103b3c5625a37e15b1427d11/LICENSE.md
-import { batch } from './helpers/batch';
+import { unstable_batchedUpdates as batch } from './helpers/batch';
 import { setBatch } from './helpers/batch-helpers';
 
 // For the normal entrypoint, we will set batch to either React or React-Native unstable_batchedUpdates

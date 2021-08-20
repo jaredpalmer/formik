59348156c8dbc23b8ce87df0b4cada5aff16ccf9
John Rom <john@nimblelight.com>
2021-08-20 17:58:45 -0400

    Manually build a native version with tsdx, move it to index.native.js, then build a web version.

--- packages/formik/src/index.native.tsx
+++ packages/formik/src/index.native.tsx
@@ -1,9 +1,14 @@
+export * from './exports';
+
 // Borrowed from React-Redux, License MIT.
 // https://github.com/reduxjs/react-redux/blob/d4e4eba9ccbd488b103b3c5625a37e15b1427d11/src/utils/reactBatchedUpdates.native.ts
 // https://github.com/reduxjs/react-redux/blob/d4e4eba9ccbd488b103b3c5625a37e15b1427d11/LICENSE.md
 //
 // The Native compiler will detect this file with a .native suffix, and inject it in place of batch.ts.
 //
-import { unstable_batchedUpdates } from 'react-native'
+import { unstable_batchedUpdates } from 'react-native';
+import { setBatch } from './helpers/batch-helpers';
 
-export { unstable_batchedUpdates }
+// For the normal entrypoint, we will set batch to either React or React-Native unstable_batchedUpdates
+// Devs using other renderers should import './index.alternate.js'; which doesn't use any optimizations.
+setBatch(unstable_batchedUpdates);
--- packages/formik/src/index.tsx
+++ packages/formik/src/index.tsx
@@ -1,11 +1,14 @@
 export * from './exports';
 
-// Borrowed from React-Redux, License MIT
-// https://github.com/reduxjs/react-redux/blob/d4e4eba9ccbd488b103b3c5625a37e15b1427d11/src/alternate-renderers.ts
+// Borrowed from React-Redux, License MIT.
+// https://github.com/reduxjs/react-redux/blob/d4e4eba9ccbd488b103b3c5625a37e15b1427d11/src/utils/reactBatchedUpdates.native.ts
 // https://github.com/reduxjs/react-redux/blob/d4e4eba9ccbd488b103b3c5625a37e15b1427d11/LICENSE.md
-import { unstable_batchedUpdates as batch } from './helpers/batch';
+//
+// The Native compiler will detect this file with a .native suffix, and inject it in place of batch.ts.
+//
+import { unstable_batchedUpdates } from 'react-dom';
 import { setBatch } from './helpers/batch-helpers';
 
 // For the normal entrypoint, we will set batch to either React or React-Native unstable_batchedUpdates
-// Devs using other renderers should import './alternate-renderers'; which doesn't use any optimizations.
-setBatch(batch);
+// Devs using other renderers should import './index.alternate.js'; which doesn't use any optimizations.
+setBatch(unstable_batchedUpdates);

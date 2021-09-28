6458b65c4dc61003d77c9f208e7adde069924baf
Jared Palmer <jared@jaredpalmer.com>
2021-09-28 14:40:16 -0400

    Report Web Vitals to GA

--- website/src/pages/_app.js
+++ website/src/pages/_app.js
@@ -6,6 +6,19 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import * as gtag from 'utils/analytics';
 
+
+export function reportWebVitals({ id, name, label, value }) {
+  // Use `window.gtag` if you initialized Google Analytics as this example:
+  // https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_document.js
+  (window as any).gtag('event', name, {
+    event_category:
+      label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    event_label: id, // id unique to current page load
+    non_interaction: true, // avoids affecting bounce rate.
+  });
+}
+
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   useEffect(() => {

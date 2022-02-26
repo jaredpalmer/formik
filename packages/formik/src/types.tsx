cabc210c918b391b7faed76d43a754f4c09dce5b
Michael Romashov <mromashov@icloud.com>
2022-02-26 14:18:04 -0500

    docs: validateAfterSubmit prop usage and behavior

--- packages/formik/src/types.tsx
+++ packages/formik/src/types.tsx
@@ -160,7 +160,7 @@ export interface FormikSharedConfig<Props = {}> {
   validateOnBlur?: boolean;
   /** Tells Formik to validate upon mount */
   validateOnMount?: boolean;
-  /** Tells Formik to only validate after the initial submit event */
+  /** Tells Formik to only validate after the initial submission attempt */
   validateAfterSubmit?: boolean;
   /** Tell Formik if initial form values are valid or not on first render */
   isInitialValid?: boolean | ((props: Props) => boolean);

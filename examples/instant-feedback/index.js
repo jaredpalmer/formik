f9047173587f327d624a2b014fc3fc2c74ffd543
Ian Gray <90417534+IanZGray@users.noreply.github.com>
2022-06-07 15:21:06 -0400

    initial commit

--- examples/instant-feedback/index.js
+++ examples/instant-feedback/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { useFormik, FormikProvider, Form, useField } from 'formik';
-import './styles.css';
-import * as Yup from 'yup';
+import React from "react";
+import ReactDOM from "react-dom";
+import { useFormik, FormikProvider, Form, useField } from "formik";
+import "./styles.css";
+import * as Yup from "yup";
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
@@ -20,18 +20,18 @@ const TextInputLiveFeedback = ({ label, helpText, ...props }) => {
   return (
     <div
       className={`form-control ${
-        showFeedback ? (meta.error ? 'invalid' : 'valid') : ''
+        showFeedback ? (meta.error ? "invalid" : "valid") : ""
       }`}
     >
       <div className="flex items-center space-between">
-        <label htmlFor={props.id}>{label}</label>{' '}
+        <label htmlFor={props.id}>{label}</label>{" "}
         {showFeedback ? (
           <div
             id={`${props.id}-feedback`}
             aria-live="polite"
             className="feedback text-sm"
           >
-            {meta.error ? meta.error : '✓'}
+            {meta.error ? meta.error : "✓"}
           </div>
         ) : null}
       </div>
@@ -51,7 +51,8 @@ const TextInputLiveFeedback = ({ label, helpText, ...props }) => {
 const Example = () => {
   const formik = useFormik({
     initialValues: {
-      username: '',
+      username: "",
+      password: ""
     },
     onSubmit: async (values) => {
       await sleep(500);
@@ -59,14 +60,22 @@ const Example = () => {
     },
     validationSchema: Yup.object({
       username: Yup.string()
-        .min(8, 'Must be at least 8 characters')
-        .max(20, 'Must be less  than 20 characters')
-        .required('Username is required')
+        .min(8, "Must be at least 8 characters")
+        .max(20, "Must be less  than 20 characters")
+        .required("Username is required")
         .matches(
           /^[a-zA-Z0-9]+$/,
-          'Cannot contain special characters or spaces'
+          "Cannot contain special characters or spaces"
         ),
-    }),
+      password: Yup.string()
+        .min(8, "Must be at least 8 characters")
+        .max(20, "Must be less  than 20 characters")
+        .required("Password is required")
+        .matches(
+          /(?=.*[0-9])/,
+          "Must contain a special character, or a number."
+        )
+    })
   });
 
   return (
@@ -76,7 +85,14 @@ const Example = () => {
           label="Username"
           id="username"
           name="username"
-          helpText="Must be 8-20 characters and cannot contain special characters."
+          helpText="Must be 8-20 characters and CANNOT contain special characters."
+          type="text"
+        />
+        <TextInputLiveFeedback
+          label="Password"
+          id="password"
+          name="password"
+          helpText="Must be 8-20 characters and MUST contain a special character or number."
           type="text"
         />
         <div>
@@ -90,41 +106,20 @@ const Example = () => {
 
 ReactDOM.render(
   <div className="app">
-    <h1 className="text-4xl">Accessible instant feeback with Formik 2</h1>
+    <h1 className="text-4xl">Remember Your Login</h1>
     <p className="text-lg">
-      Instant feedback during typing can be extremely helpful in certain
-      situations. For example, checking the validity (or availability) of a
-      username shouldn't require the user to resubmit the form (multiple times).
-      Providing instant feedback while users are typing can allow them to
-      experiment more easily until they find valid input value (like a suitable
-      username). Note: This isn't always optimal, use your judgement.
+      With different requirements on different pages, who can remember which
+      version of your password is used on which site? By displaying the password
+      requirements prior to login, you can be remindded that your 'Password1'
+      won't work here. Oh, then you must have made the password 'Password1!'
+      instead. No need to constantly reset them. Maybe if more forms acted like
+      this, we'd not need to keep resetting them, and actually start remembering
+      our passwords in the first place.
     </p>
+
     <div className="example">
       <Example />
     </div>
-    <p className="text-md mt-1">
-      <strong>Note:</strong> The displayed feedback message in this example is
-      coded using a <code>{`<div>`}</code> element that has an{' '}
-      <code>{`aria-live`}</code> attribute with the value <code>polite</code>.
-      The contents of this so called "live region" are conveyed to screen
-      readers and other assistive technology. The value "polite" de-emphasizes
-      the importance of the message and does not cause screen readers to
-      interrupt their current tasks to read aloud this message. Thus the message
-      is only read once when the user stops typing rather than on every
-      keystroke that the user makes.
-    </p>
-    <p className="text-sm">
-      <i>
-        Example adapted from{' '}
-        <a
-          href="https://www.w3.org/WAI/tutorials/forms/notifications/"
-          target="blank"
-          rel="noopener noreferrer"
-        >
-          W3C WAI Web Accessibility Tutorials
-        </a>
-      </i>
-    </p>
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );

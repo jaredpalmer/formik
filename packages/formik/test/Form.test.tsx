92559cfc079c0e551b5ad16cbc4b581a66d9c18f
Kostiantyn Popovych <kostiantyn.popovych.exp@gmail.com>
2022-08-11 15:37:10 -0700

    [#3590] Get rid of not needed func import

--- packages/formik/test/Form.test.tsx
+++ packages/formik/test/Form.test.tsx
@@ -1,23 +1,17 @@
-import {
-  act,
-  fireEvent,
-  render,
-  screen,
-  waitFor,
-} from '@testing-library/react';
+import { act, fireEvent, render, waitFor } from '@testing-library/react';
 import { Form, Formik } from '../src';
-import { ENTER_KEY_CODE } from '../src/Form';
+import { ENTER_KEY_CODE } from '../src';
 import * as React from 'react';
 
 describe('<Form />', () => {
   it('should submit form 5 times while holding enter key pressed', async () => {
     const onSubmit = jest.fn();
-    render(
+    const { getByRole } = render(
       <Formik initialValues={{}} onSubmit={onSubmit}>
         <Form name="Form" />
       </Formik>
     );
-    const form = screen.getByRole('form');
+    const form = getByRole('form');
 
     act(() => {
       fireEvent.submit(form);
@@ -32,12 +26,12 @@ describe('<Form />', () => {
 
   it('should submit form two times when pressing enter key, then unpressing it & repeat', async () => {
     const onSubmit = jest.fn();
-    render(
+    const { getByRole } = render(
       <Formik initialValues={{}} preventStickingSubmissions onSubmit={onSubmit}>
         <Form name="Form" />
       </Formik>
     );
-    const form = screen.getByRole('form');
+    const form = getByRole('form');
 
     act(() => {
       fireEvent.submit(form);
@@ -54,12 +48,12 @@ describe('<Form />', () => {
 
   it('should submit form only once while holding enter key pressed', async () => {
     const onSubmit = jest.fn();
-    render(
+    const { getByRole } = render(
       <Formik initialValues={{}} preventStickingSubmissions onSubmit={onSubmit}>
         <Form name="Form" />
       </Formik>
     );
-    const form = screen.getByRole('form');
+    const form = getByRole('form');
 
     act(() => {
       fireEvent.submit(form);

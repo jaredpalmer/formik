214a563e00ba9442188c2415ef84451528213153
SvyatoslavPozhydaev <svyatreal@gmail.com>
2023-05-10 12:24:27 +0300

    fix types for Field

--- packages/formik/src/Field.tsx
+++ packages/formik/src/Field.tsx
@@ -69,7 +69,7 @@ export interface FieldConfig<V = any> {
 
 export type FieldAttributes<T> = GenericFieldHTMLAttributes &
   FieldConfig<T> &
-  T & { name: string };
+  { name: string };
 
 export type FieldHookConfig<T> = GenericFieldHTMLAttributes & FieldConfig<T>;
 

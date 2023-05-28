4abfbffe36ef77f60fb403ff777280b68d163f64
Jared Palmer <jared@jaredpalmer.com>
2023-05-28 14:39:02 -0400

    Use .blur() in playwright (h/t chatgpt for messing this up)

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start:app',
-    url: 'http://127.0.0.1:3000',
+    url: 'http://127.0.0.1:3001',
     reuseExistingServer: !process.env.CI,
   },
 });

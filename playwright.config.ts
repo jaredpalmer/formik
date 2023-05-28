b1cd8597a60a1a5132dc7dd25617b4a35b53091e
Jared Palmer <jared@jaredpalmer.com>
2023-05-28 17:37:52 -0400

    Use correct port in playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start:app',
-    url: 'http://127.0.0.1:3001',
+    url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
   },
 });

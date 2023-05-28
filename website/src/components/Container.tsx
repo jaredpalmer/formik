2a61e0db3c96805666ed4c9630f3fc5925c060b3
Jared Palmer <jared@jaredpalmer.com>
2023-05-28 13:07:25 -0400

    Move to workspaces

--- website/src/components/Container.tsx
+++ website/src/components/Container.tsx
@@ -4,7 +4,10 @@ import { HTMLProps } from 'react';
 
 export type ContainerProps = HTMLProps<HTMLDivElement>;
 
-export const Container: React.FC<ContainerProps> = ({ className, ...props }) => {
+export const Container: React.FC<ContainerProps> = ({
+  className,
+  ...props
+}) => {
   return <div className={cn('container mx-auto', className)} {...props} />;
 };
 

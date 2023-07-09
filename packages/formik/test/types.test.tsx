ac5018d8f633dc633917b93d29d24dc311a05ef6
Viktor Chernodub <chernodubv@gmail.com>
2023-07-09 15:14:39 +0300

    test: add tests for typed fields

--- packages/formik/test/types.test.tsx
+++ packages/formik/test/types.test.tsx
@@ -1,4 +1,4 @@
-import { FormikTouched, FormikErrors } from '../src';
+import { FormikErrors, FormikProps, FormikTouched } from '../src';
 
 describe('Formik Types', () => {
   describe('FormikTouched', () => {
@@ -43,4 +43,45 @@ describe('Formik Types', () => {
       expect(facebook).toBe('error');
     });
   });
+
+  it('FormikProps', () => {
+    type Values = {
+      user: { 
+        lastName: string; 
+        firstName: string; 
+        10: string;
+      };
+      action: 'testing';
+    };
+    type Props = FormikProps<Values>;
+    
+    function expectType<T>(_: T) {
+      expect(true).toBe(true);
+    }
+
+    // @ts-expect-error
+    expectType<Parameters<Props['getFieldHelpers']>[0]>('unexpected');
+      
+    // @ts-expect-error
+    expectType<Parameters<Props['getFieldMeta']>[0]>('randomText');
+
+    // @ts-expect-error
+    expectType<Parameters<Props['getFieldProps']>[0]>('');
+
+    // @ts-expect-error
+    expectType<Parameters<Props['setFieldValue']>[0]>('ucer');
+
+    // @ts-expect-error
+    expectType<Parameters<Props['setFieldError']>[0]>('firstName');
+
+    // @ts-expect-error
+    expectType<Parameters<Props['setFieldTouched']>[0]>('action10');
+
+    expectType<Parameters<Props['getFieldHelpers']>[0]>('user');
+    expectType<Parameters<Props['getFieldMeta']>[0]>('action');
+    expectType<Parameters<Props['getFieldProps']>[0]>('user.firstName');
+    expectType<Parameters<Props['setFieldValue']>[0]>('user.lastName');
+    expectType<Parameters<Props['setFieldError']>[0]>('user.10');
+    expectType<Parameters<Props['setFieldTouched']>[0]>('action');
+  })
 });
